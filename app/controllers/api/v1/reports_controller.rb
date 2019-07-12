abf9cd596acd9f0f7b4c7aaf48f920b34741ae34
Fredrick Chiyenda <fchiyenda@gmail.com>
2019-07-12 16:21:00 +0200

    Completed Case listing API

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -1,16 +1,13 @@
 class Api::V1::ReportsController < ApplicationController
 
  def cbs_case_listing
-  start_date, end_date = params.require %i[start_date end_date]
-  cases = service.cbs_case_listing(start_date, end_date)
-
+  cases = service.cbs_case_listing
   render json: cases
  end
 
- private
-
- def service
- 	
+  def service
+    ReportService.new(start_date: params[:start_date].to_date, 
+        end_date: params[:end_date].to_date, district_id: 1, site_id: 1)
  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'authentication#authenticate'
+  namespace :api do
+    namespace :v1 do
+      post '/reports/case_listing', to: '/api/v1/reports#cbs_case_listing'
+    end
+  end
 end

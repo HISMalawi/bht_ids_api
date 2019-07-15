19cb6ca4b552584601fff2ff7ab9902517266e31
taonam <taochipeta@gmail.com>
2019-07-15 12:38:40 +0200

    working on viral load

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::ReportsController < ApplicationController
 
  def cbs_case_listing
-  cases = service.cbs_case_listing
-  render json: cases
+    cases = service.cbs_case_listing
+    render json: cases
  end
 
-  def service
+ # def art_initiation_report
+ # 	initiated = service.cbs_art_initiated
+ # 	render json:initiated
+ # end
+
+ def service
     ReportService.new(start_date: params[:start_date].to_date, 
         end_date: params[:end_date].to_date, district_id: 1, site_id: 1)
  end
